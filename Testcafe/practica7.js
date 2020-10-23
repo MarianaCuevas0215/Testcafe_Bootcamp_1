9103273b5db3780639b258d32ff125ee2540568d
MarianaCuevas0215 <marpj_0215@hotmail.com>
2020-10-22 21:19:39 -0500

    Practicas de la sesión del 22 de Octubre

---
+++ Testcafe/practica7.js
@@ -0,0 +1,28 @@
+import page from './pageModel';
+fixture('Test con testcafe')
+    .page ('https://formy-project.herokuapp.com/form');
+
+test('Validar un formulario', async t =>{
+    await t
+        .typeText(page.input71, "Mariana", {speed:0.1})
+        .expect(page.input71.value).eql("Mariana")
+        .typeText(page.input72, "Cuevas", {speed:0.1})
+        .expect(page.input72.value).eql("Cuevas")
+        .typeText(page.input73, "Software engineer in test")
+        .expect(page.input73.value).eql("Software engineer in test")
+        .click(page.rbt74)
+        .expect(page.rbt74.checked).ok()
+        .click(page.rbt75)
+        .expect(page.rbt75.checked).ok()
+        .click(page.checkbox77)
+        .expect(page.checkbox77.checked).ok()
+        .click(page.checkbox78)
+        .expect(page.checkbox78.checked).ok()
+        .click(page.checkbox79)
+        .expect(page.checkbox79.checked).ok()
+        .click(page.smenu701)
+        .click(page.opt702)
+        .typeText(page.date703, "01112020")
+        .click(page.btn704)
+        
+})
